79e6c41aae0353c7920a1a7528578d95a9a3e05c
Simon Smith <simonsmith@users.noreply.github.com>
2018-03-30 17:35:55 +0100

    Use correct path the jquery library

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleNameMapper: {
-    jquery: '<rootDir>/example/jquery-1.12.4.js',
+    jquery: '<rootDir>/test/assets/jquery-1.12.4.js',
   },
   testRegex: 'test/.*\\.spec\\.js$',
 };
